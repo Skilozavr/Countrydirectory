aaa859dc644ef10d3cebe16aa0a45496fdd88b48
Skilozavr <skillozzavrik@gmail.com>
2022-07-22 21:33:07 +0300

    Creating project

---
+++ app/src/main/java/com/example/countrydirectory/RemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.countrydirectory
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+    interface RestCountriesApi {
+        @GET("name/{name}")
+        suspend fun getCountryByName(@Path("name") cityName: String): List<Country>
+    }
+
+    var retrofit = Retrofit.Builder()
+        .baseUrl("https://countriesinfo21.herokuapp.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    var restCountriesApi = retrofit.create(RestCountriesApi::class.java)
+
+
